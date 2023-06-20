9c8b6d27c9d9f071a0ea75b7d0d750ba368b8d70
unuefeejovwoke <vuwill7114@gmail.com>
2023-06-19 21:18:37 -0700

    speaker detail done

--- speakers/views.py
+++ speakers/views.py
@@ -14,3 +14,27 @@ def speakers_list(request):
         # Add more dummy speaker information
     ]
     return render(request, 'speakers/speakers_list.html', {'speakers': speakers})
+
+
+def speaker_create(request):
+    return render(request, 'speakers/speaker_create.html')
+
+def speaker_detail(request, speaker_id):
+    speakers = [
+        {'id': 1, 'name': 'Speaker 1', 'bio': 'Speaker bio 1', 'contact': 'speaker1@example.com'},
+        {'id': 2, 'name': 'Speaker 2', 'bio': 'Speaker bio 2', 'contact': 'speaker2@example.com'},
+        {'id': 3, 'name': 'Speaker 3', 'bio': 'Speaker bio 3', 'contact': 'speaker3@example.com'},
+        {'id': 4, 'name': 'Speaker 4', 'bio': 'Speaker bio 4', 'contact': 'speaker4@example.com'},
+        {'id': 5, 'name': 'Speaker 5', 'bio': 'Speaker bio 5', 'contact': 'speaker5@example.com'},
+    ]
+
+    speaker = None
+    for spk in speakers:
+        if spk['id'] == speaker_id:
+            speaker = spk
+            break
+
+    if speaker is None:
+        raise Http404("Speaker does not exist.")
+
+    return render(request, 'speakers/speaker_detail.html', {'speaker': speaker})
